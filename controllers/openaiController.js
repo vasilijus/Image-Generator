8a5532cd039bef44f2d408840460a045038ecc78
vasilijus <vasiliokas@gmail.com>
2022-12-08 23:49:51 +0000

    setup - rephrase names

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -2,7 +2,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-    apiKey: process.env.API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
