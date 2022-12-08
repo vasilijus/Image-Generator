5f24799a3711ef39ca87419cf97d64099cba2038
vasilijus <vasiliokas@gmail.com>
2022-12-08 23:49:15 +0000

    setup - openai

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,3 +1,11 @@
+// const dotenv = require('dotenv').config();
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+    apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateImage = async (request, response) => {
     response.status(200).json({
         success: true,
