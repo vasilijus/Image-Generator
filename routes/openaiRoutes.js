946745e472257c01fa4e68f87c5a8d788bf13d05
vasilijus <vasiliokas@gmail.com>
2022-12-08 23:39:48 +0000

    setup - controller , inline , imported

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { generateImage } = require('../controllers/openaiController');
 const router = express.Router();
 
-// http://localhost:5000/openai/generate
-router.get('/generate', (request, response) => {
+// http://localhost:5000/openai/test
+router.get('/test', (request, response) => {
     response.status(200).json({
         success: true,
     })
 })
-
-router.post('/generateImage', generateImage )
+// http://localhost:5000/openai/generateImage
+router.post('/generateImage', generateImage );
 
 module.exports = router;
