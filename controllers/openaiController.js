8a684547fabe70fa410ce82d54e9968528945109
vasilijus <vasiliokas@gmail.com>
2022-12-08 23:56:03 +0000

    setup - try catch gen

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,10 +6,25 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const generateImage = async (request, response) => {
-    response.status(200).json({
-        success: true,
-    })
+const generateImage = async (req, res) => {
+    try {
+        const response = await openai.createImage({
+            prompt: 'Russian bear on rocket',
+            n: 1,
+            size: '521x512'
+        });
+        const imageUrl = response.data.data[0].url;
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl,
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: 'The image could not be generated.'
+        })
+    }
 };
 
 module.exports = { generateImage };
