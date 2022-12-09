6009ed3a65a4dcb168ee35588dd36d8a3ae7a1c3
vasilijus <vasiliokas@gmail.com>
2022-12-09 14:15:44 +0000

    AI works well

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,52 @@
 function onSubmit(e) {
     e.preventDefault();
-
-    const prompt;
-    const size;
-
+    const prompt = document.querySelector('#prompt').value;
+    const size = document.querySelector('#size').value;
+    
     if(prompt === '') {
         alert('Please add some text');
     }
+
+    generateImageRequest(prompt, size);
 }
 
-document.querySelector('').addEventListener('submit', onSubmit);
+async function generateImageRequest(prompt, size) {
+    try {
+        showSpinner();
+
+        const response = await fetch('/openai/generateImage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt,
+                size
+            })
+        });
+        
+        if( !response.ok ) {
+            removeSpinner();
+            throw new Error('That image could not be generated.');
+        }
+
+        const data = await response.json();
+        // console.log(data);
+        const imageUrl = data.data;
+
+        
+        removeSpinner();
+    } catch (error) {
+        document.querySelector('.msg').textContent = error;
+    }
+}
+
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('loading');
+}
+function removeSpinner() {
+    document.querySelector('.spinner').classList.remove('loading');
+}
+
+document.querySelector('#image-form').addEventListener('submit', onSubmit);
+
