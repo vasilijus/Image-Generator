5826d3976fbb60ca8e1f468220cd4be64743ef98
vasilijus <vasiliokas@gmail.com>
2022-12-09 00:57:20 +0000

    HTML update for image

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+function onSubmit(e) {
+    e.preventDefault();
+
+    const prompt;
+    const size;
+
+    if(prompt === '') {
+        alert('Please add some text');
+    }
+}
+
+document.querySelector('').addEventListener('submit', onSubmit);
